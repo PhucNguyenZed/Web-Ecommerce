21840548be8c6ecced77e548244644eea91eaf8a
PhucNguyenZed <nguyendeptrai1407@gmail.com>
2022-12-05 16:42:45 +0700

    ver2

--- srcipt.js
+++ srcipt.js
@@ -45,3 +45,88 @@ var swiper = new Swiper('.swiper', {
     },
    
 });
+
+//show search
+const searchButton = document.querySelector('.t-search'),
+    tClose =document.querySelector('.search-close'),
+    showClass = document.querySelector('.site');
+searchButton.addEventListener('click',function(){
+    showClass.classList.toggle('showsearch')
+})
+tClose.addEventListener('click',function(){
+    showClass.classList.remove('showsearch')
+})
+
+//show dpt menu
+const dptButton = document.querySelector('.dpt-cat .dpt-trigger'),
+        dptClass = document.querySelector('.site');
+dptButton.addEventListener('click',function(){
+    dptClass.classList.toggle('showdpt');
+})
+
+//image slider
+
+var productThumb = new Swiper ('.small-image', {
+    loop: true,
+    spaceBetween:10,
+    slidesPerView:3,
+    freeMode:true,
+    watchSlidesProgress:true,
+    breakpoints:{
+        481: {
+            spaceBetween:32,
+        }
+    }
+
+});
+var productBig = new Swiper ('.big-image', {
+    loop: true,
+    autoHeight: true,
+    navigation: {
+        nextEl :'.swiper-button-next',
+        prevEl :'.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: productThumb,
+    }
+})
+
+//thanh bar
+
+var stocks =document.querySelectorAll('.products .stock')
+for(let x = 0;x < stocks.length ; x++) {
+    let stock =stocks[x].dataset.stock,
+    available =stocks[x].querySelector('.qty-available').innerHTML,
+    sold = stocks[x].querySelector('.qty-sold').innerHTML
+    percent =sold * 100 / stock;
+    
+    stocks[x].querySelector('.available').style.width = percent +'%';
+}
+
+//show cart on click 
+const divtoShow = '.mini-cart';
+const divPopup = document.querySelector(divtoShow);
+const divTrigger = document.querySelector('.cart-trigger');
+
+divTrigger.addEventListener('click', () => {
+    setTimeout(() => {
+        if(!divPopup.classList.contains('show')) {
+            divPopup.classList.add('show');
+        }
+    },250)
+})
+
+document.addEventListener('click', (e) => {
+    const isCLosest = e.target.closest(divtoShow);
+    if(!isCLosest && divPopup.classList.contains('show')){
+        divPopup.classList.remove('show');
+    }
+})
+/*  show Modal */
+
+window.onload = function () {
+    document.querySelector('.site').classList.toggle('showmodal')
+}
+document.querySelector('.modalclose').addEventListener('click', function () {
+    document.querySelector('.site').classList.remove('showmodal')
+})
